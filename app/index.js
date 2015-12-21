e2c0bb1648334134d59bb45fc5f9e0ec77fdc7a7
empretech <empretech.art@gmail.com>
2015-12-21 14:17:40 -0600

    first commit, adding files to repo

---
+++ app/index.js
@@ -0,0 +1,32 @@
+#main-header
+{
+	height: 117px;
+	position: relative;
+	z-index: 10;
+	background-color:#59DBDB;
+}
+
+#main-navigation
+{
+	height: 117px;
+	position: relative;
+	z-index: 10;
+	background-color:#59DBDB;
+}
+
+#main-blog
+{
+	height: 117px;
+	position: relative;
+	z-index: 10;
+	background-color:#59DBDB;
+}
+
+#main-section
+{
+	height: 117px;
+	position: relative;
+	z-index: 10;
+	background-color:#59DBDB;
+}
+
