dc7c791b9c1e5ade78e2fa1eb385a7152163033c
empretech <empretech.art@gmail.com>
2016-01-03 04:06:33 -0600

    practica terminada

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
+
 (function myBlog(){
 
-  var head="variable1";
-  var par="contenido del parafo";
-
   var app = {
-    myDOMapi: domApiFunc(),
-    addSections: addSectionsFunc,
-    mainContainer: null,
+	  
+	DOMapi: domApiFunc(),
+	DataApi: dataApiFunc(),
+	addMenu: addMenuFunc,
+	addSections: addSectionsFunc,
+	registerEvents: registerEventsFunc,
+	
+	mainSectionsContainer: null,
+	sections: null,
+	menu: null,
     init: init
 }
 
@@ -14,46 +19,142 @@ app.init();
 
 function init() {
 
+  this.addMenu();
   this.addSections();
+  this.registerEvents();
 }
 
-function addSectionsFunc(){
-   this.mainContainer = this.myDOMapi.getSectionContainer('main-sections-container');
-  var sections = [   //do request for sections(AJAX)
-    '<section><header><h2>'+head+'</h2></header><div class="full"><span>more</span></div><article><img src="android-5-0-lollipop-material-2961.jpg" alt="Terrific new"><p>'+par+'</p></article></section><br/>',
-    '<section><header><h2>terrific new</h2></header><div class="full"><span>more</span></div><article><img src="android-5-0-lollipop-material-2961.jpg" alt="Terrific new"><p></p></article></section><br/>',
-    '<section><header><h2>terrific new</h2></header><div class="full"><span>more</span></div><article><img src="android-5-0-lollipop-material-2961.jpg" alt="Terrific new"><p></p></article></section><br/>'
-    ];
-    function addItemHTML(item){
-      this.mainContainer.innerHTML += item;
-    }
-    this.myDOMapi.addItems(sections, addItemHTML.bind(this));
-}
 
-/*function updateArticleText(){
-  var sections = this.myDOMapi.getSections('.section');
-  for (var i = i < section.length; i++) {
-    var els = sections[i].children[3].getElementsByTagName('p');
-    for (var x = x < section.length; x++){
-      els[x].textContent = "lorem";
-    };
-  };
-}*/
+function addMenuFunc() {
+	
+	function constructMenu() {
+		
+		var container = this.DOMapi.getContainer("main-nav");
+		var newNav = document.createElement("nav");
+		var newList = document.createElement("ul");
+		newNav.appendChild(newList);		
+		container.appendChild(newNav);
 
-function domApiFunc(){
-  function getSectionContainer(id){
-    return document.getElementById(id);
-  }
-  function addItems(items, callBack){
-    for (var i = 0; i < items.length; i++) {
-        callBack(items[i]);
-    };
-  }
-  var publicAPI = {
-    getSectionContainer: getSectionContainer,
-    addItems: addItems
-  }
-  return publicAPI;
+		var containerMov = this.DOMapi.getContainer("menu-mobile-container");
+		var newNavMov = document.createElement("nav");
+		var newListMov = document.createElement("ul");
+		newNavMov.appendChild(newListMov);		
+		containerMov.appendChild(newNavMov);
+
+
+		function addList(item, index) {
+			var index = index + 1;
+			newList.innerHTML += "<li>"+(item.title + " " + index)+"</li>";
+			newListMov.innerHTML += "<li>"+(item.title + " " + index)+"</li>";
+		}
+
+		this.DOMapi.addItems(this.menu, addList)
+	}
+
+	function addMenuToDOM(obj) {
+		this.sections = obj.data.sections;
+		this.menu = obj.data.menu;
+		constructMenu.call(this);
+	}
+
+	this.DataApi.getData(addMenuToDOM.bind(this));
 };
 
-})();
+
+function addSectionsFunc() {
+	
+	function constructSections() {
+		
+			var container = this.DOMapi.getContainer("main-sections-container");
+			
+			function addSections(item, index) {
+
+				container.innerHTML+="<section><header><h2>"+(item.title)+"</h2></header><div class=\"full\"><span>more</span></div><article><img src=\""+(item.image)+"\"><p>"+(item.art)+"</p></article></section></br>";
+			}
+
+
+			this.DOMapi.addItems(this.sections, addSections)
+	}
+
+	function addSectionsToDOM(obj) {
+		this.sections = obj.data.sections;
+		constructSections.call(this);
+	}
+
+	this.DataApi.getData(addSectionsToDOM.bind(this));
+};
+
+
+function domApiFunc() {
+
+	function getContainer(id) {		
+		return document.getElementById(id);
+	}
+
+	function addItems(items, callBack) {
+		for (var i = 0; i < items.length; i++) {
+			callBack(items[i], i);
+		};
+	}
+
+	var publicAPI = {
+		getContainer: getContainer,
+		addItems: addItems
+	}
+
+	return publicAPI;
+};
+
+function dataApiFunc() {
+
+	var URLs = {
+		get: "data/sections.json",
+		post: "nothing yet"
+	};
+
+	function getData(callBack) {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				callBack(JSON.parse(xmlhttp.responseText));
+			};
+		};
+
+		xmlhttp.open("GET", URLs.get, true);
+		xmlhttp.send();
+	}
+
+	function sendData() {
+	//code to send data 
+	//to server/WS
+	}
+
+	var publicAPI = {
+		getData: getData,
+		sendData: sendData
+	}
+
+	return publicAPI;
+};
+
+function registerEventsFunc () {
+	var menuMobileBtn = document.querySelector("#menu-mobile-btn"),
+	menuMobileContainer = document.querySelector("#menu-mobile-container"),
+	bodyTag = document.getElementsByTagName('body')[0],
+	target = null;
+
+	menuMobileBtn.addEventListener("touchstart", function (event) {
+		target = event.target.localName === 'span' ? event.target.parentElement : event.target
+		if (target.classList.length === 0) {
+			bodyTag.classList.add('no-scroll');
+			menuMobileContainer.classList.add('open');
+			target.classList.add('open');
+		}else {
+			bodyTag.classList.remove('no-scroll');
+			menuMobileContainer.classList.remove('open');
+			target.classList.remove('open');
+		};
+	}, false);
+}
+
+})(); 
